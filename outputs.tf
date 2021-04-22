0b25b6f745b85841c90d73396422d415b8a6f797
ritsok <rita@hashicorp.com>
2021-04-22 12:05:28 -0400

    Initial commit

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+output "cluster_id" {
+  description = "EKS cluster ID."
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = local.cluster_name
+}
+
+output "cluster_certificate_authority" {
+  description = "Cluster certificate authority"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
