b41567fc2eb3d2132b993c0a67e0f99161e772ae
ritsok <rita@hashicorp.com>
2021-05-04 15:21:51 -0400

    update readme and outputs

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learn Terraform - Deploy Federated Multi-Cloud Kubernetes Clusters
+
+This is a companion repository with part of the configuration for the [Deploy
+Federated Multi-Cloud Kubernetes Clusters
+guide](https://learn.hashicorp.com/tutorials/terraform/multicloud-kubernetes)
+on [HashiCorp Learn](https://learn.hashicorp.com/).  It contains Terraform
+configuration files for you to use to learn how to provision an EKS cluster.
--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "cluster_id" {
   value       = module.eks.cluster_id
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
-
 output "region" {
   description = "AWS region"
   value       = var.region
@@ -16,10 +11,4 @@ output "region" {
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.cluster_name
-}
-
-output "cluster_certificate_authority" {
-  description = "Cluster certificate authority"
-  value       = module.eks.cluster_certificate_authority_data
-  sensitive   = true
 }
